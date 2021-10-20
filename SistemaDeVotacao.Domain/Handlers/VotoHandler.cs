a3a8fcbcb3392bcacb4c4bffabbdd2551049c772
Luis Carlos Basso Saraiva <lucarbass@LIVE.COM>
2021-10-20 09:57:51 -0300

    Adicionado validação para usuário e filme no voto

--- SistemaDeVotacao.Domain/Handlers/VotoHandler.cs
+++ SistemaDeVotacao.Domain/Handlers/VotoHandler.cs
@@ -10,10 +10,14 @@ namespace SistemaDeVotacao.Domain.Handlers
     public class VotoHandler : ICommandHandler<AdicionarVotoCommand>, ICommandHandler<ExcluirVotoCommand>
     {
         private readonly IVotoRepository _repository;
+        private readonly IUsuarioRepository _usuarioRepository;
+        private readonly IFilmeRepository _filmeRepository;
 
-        public VotoHandler(IVotoRepository repository)
+        public VotoHandler(IVotoRepository repository, IUsuarioRepository usuarioRepository, IFilmeRepository filmeRepository)
         {
             _repository = repository;
+            _usuarioRepository = usuarioRepository;
+            _filmeRepository = filmeRepository;
         }
 
         public ICommandResult Handle(AdicionarVotoCommand command)
@@ -23,6 +27,13 @@ namespace SistemaDeVotacao.Domain.Handlers
                 if (!command.ValidarCommand())
                     return new VotoCommandResult(false, "Por favor corrija as inconsistências.",
                         command.Notifications);
+                
+                if (!_usuarioRepository.CheckId(command.IdUsuario))
+                    return new VotoCommandResult(false, "Este usuário não existe.", new { });
+                
+                if (!_filmeRepository.CheckId(command.IdFilme))
+                    return new VotoCommandResult(false, "Este filme não existe.", new { });
+
                 long id = 0;
                 id = _repository.Inserir(new Voto(id, command.IdUsuario, command.IdFilme));
                 return new VotoCommandResult(true, "Voto computado com sucesso!", new {Id = id});
